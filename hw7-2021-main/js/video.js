fe77072f3caa841efbde9fd380bcf7c4bed0eef2
vixjwang-byte <vixjwang@umich.edu>
2025-11-05 09:47:01 -0500

    Update video.js

--- hw7-2021-main/js/video.js
+++ hw7-2021-main/js/video.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	console.log("Volume is " + video.volume * 100 + "%");
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 	video.play();
 });
 
