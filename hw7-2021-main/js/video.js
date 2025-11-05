5b33132321f258dc5a6cd9f32eb51002d0e2a112
vixjwang-byte <vixjwang@umich.edu>
2025-11-05 09:21:47 -0500

    Add files via upload

---
+++ hw7-2021-main/js/video.js
@@ -0,0 +1,65 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video");
+	video.playbackRate *= 0.9;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function(){
+	console.log("Speed Up Video");
+	video.playbackRate *= 1.1;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function(){
+	console.log("Skip Ahead");
+	if (video.currentTime + 10 >= video.duration){
+		video.currentTime = 0;
+	} else {
+		video.currentTime += 10;
+	}
+	console.log("Current time is " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function(){
+	if (video.muted == false){
+		video.muted = true;
+		console.log("Muted");
+	} else {
+		video.muted = false;
+		console.log("Unmuted");
+	}
+});
+
+document.querySelector("#slider").addEventListener("input", function(){
+	console.log("Volume Slider");
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+	console.log("Volume is " + video.volume * 100 + "%");
+});
+
+document.querySelector("#vintage").addEventListener("click", function(){
+	console.log("Old School");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function(){
+	console.log("Original");
+	video.classList.remove("oldSchool");
+});
